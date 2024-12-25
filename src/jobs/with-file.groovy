65d970815d81c45d41b8d9a8679fc50c72356a2f
yamenw <yamentaad@gmail.com>
2024-12-25 11:17:10 +0200

    add example with jenkinsfile

---
+++ src/jobs/with-file.groovy
@@ -0,0 +1,17 @@
+pipelineJob('minimal-pipeline-job') {
+    description('Example with a Jenkinsfile')
+
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url('https://github.com/yamenw/jenkins-demo.git')
+                    }
+                    branch('main')
+                }
+            }
+            scriptPath('Jenkinsfile')
+        }
+    }
+}
